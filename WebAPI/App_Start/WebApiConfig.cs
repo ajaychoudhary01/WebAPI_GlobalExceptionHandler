1d6e4c5cf38945d757f96f777e1117fe95426ac5
Ajay Choudhary <ajaychoudhary091989@gmail.com>
2018-10-01 19:37:13 +0530

    Update WebApiConfig.cs

--- WebAPI/App_Start/WebApiConfig.cs
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using System.Web.Http.ExceptionHandling;
-using WebAPI.ExceptionFilter;
 using WebAPI.ExceptionLogger;
 using WebAPI.GlobalExceptionFilter;
 
